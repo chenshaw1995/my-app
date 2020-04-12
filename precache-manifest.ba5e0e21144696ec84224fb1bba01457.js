da4abf2b32786b34170e190042508028737ec86c
chenxichao <chenxichao1995@outlook.com>
2020-04-12 10:18:17 -0700

    Updates

---
+++ precache-manifest.ba5e0e21144696ec84224fb1bba01457.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03f2e723da6b02362012a3ca107473ce",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "21a3f455ab2cbdecc930",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c894e8a8e01d6a599ce8",
+    "url": "/my-app/static/js/2.07a05dc4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.07a05dc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21a3f455ab2cbdecc930",
+    "url": "/my-app/static/js/main.8b726371.chunk.js"
+  },
+  {
+    "revision": "7017a0a3783b2a9ce739",
+    "url": "/my-app/static/js/runtime-main.b51f6de3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
