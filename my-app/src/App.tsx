73713b03a487aef834316eabaf3592f14b641bd7
chenxichao <chenxichao1995@outlook.com>
2020-04-17 00:10:35 -0700

    router

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import FrontPage from './components/FrontPage';
+import Resume from './components/Resume';
 import './App.css';
+import {
+  Switch,
+  Route,
+  HashRouter
+} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HashRouter>
+        <Route exact path='/'>
+          <FrontPage />
+        </Route>
+        <Switch>
+          <Route path='/resume'>
+            <Resume />
+          </Route>
+        </Switch>
+      </HashRouter>
     </div>
   );
 }
---
+++ my-app/src/components/Resume.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export const Resume = ({}) => {
+    return (
+        <div>
+            This will be my resume.
+        </div>
+    )
+}
+
+export default Resume;
